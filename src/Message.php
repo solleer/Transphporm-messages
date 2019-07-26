7e9efce624eeaed4b2e2e0ea62a0729f6c958f3b
Richard <solleer@hotmail.com>
2019-07-26 16:16:23 -0400

    Allow passing an array to add multiple messages

--- src/Message.php
+++ src/Message.php
@@ -2,7 +2,12 @@
 namespace TransphpormMessages;
 class Message implements \Transphporm\Property {
     public function run(array $values, \DomElement $element, array $rules, \Transphporm\Hook\PseudoMatcher $pseudoMatcher, array $properties = []) {
-        $_SESSION['messages'][] = ["message" => $values[0], "type" => $this->getType($values)];
+        if (is_array($values[0]) foreach ($values[0] as $message) $this->addMessage($message, $this->getType($values));
+        else $this->addMessage($values[0], $this->getType($values));
+    }
+    
+    private function addMessage($message, $type) {
+        $_SESSION['messages'][] = ["message" => $message, "type" => $type];
     }
 
     private function getType($values) {
